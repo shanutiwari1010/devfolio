a789e201b7fd3b7d0c40adb3b81d82cbee2c0122
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-09-06 02:03:58 +0530

    feat: added header

---
+++ src/components/header/index.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="h-16 inline-flex items-center justify-center">
+      <img src="/logo.svg" alt="logo-img" className="ml-[89px] w-fit h-fit" />
+    </header>
+  );
+};
+
+export default Header;
