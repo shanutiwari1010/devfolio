7239d1857156f100223c01f7d41cbb84a94af58b
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-09-06 02:02:08 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/header";
+import "./App.css";
+import Hero from "./components/hero";
+
+function App() {
+
+  return (
+    <div className="h-screen">
+      <Header />
+      <Hero/>
+      
+    </div>
+  );
+}
+
+export default App;
